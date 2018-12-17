03db93b2e9d827310ffbe4372e06c74fa7895d2a
jioongyy <36102827+jioongyy@users.noreply.github.com>
2018-12-17 17:23:34 +0800

    添加多线程，底层情况优化
    数组长度<25时不再归并，数组长度<500000时不再创建线程递归

--- main.cpp
+++ main.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 #include <thread>
 #include <omp.h>
+#include <algorithm>
+#include <ctime>
 using namespace std;
 
 int res[100000000];
@@ -35,12 +37,25 @@ void myMerge(int l,int r)
 
 void MergeSort(int l,int r)
 {
-	
-	if (r - l == 1)	return;
-	int mid = l + (r - l) / 2;
+	//底层情况不再递归
+	if (r - l <= 25)
 	{
-	MergeSort(l, mid);
-	MergeSort(mid, r); 
+		sort(res + l, res + r);
+		return;
+	}
+	int mid = l + (r - l) / 2;
+	//数组长度过小时不再创建线程，单线程内跑递归
+	if (r - l >= 500000)
+	{
+		thread tl(MergeSort, l, mid);
+		thread tr(MergeSort, mid, r);
+		tl.join();
+		tr.join();
+	}
+	else
+	{
+		MergeSort(l, mid);
+		MergeSort(mid, r);
 	}
 	myMerge(l, r);
 }
@@ -50,12 +65,15 @@ int main()
 	int count = 0;
 	while (scanf_s("%d",&res[count++])!=EOF);
 	//while (cin>>res[count++]);
+	clock_t start, finish;
+	start = clock();
 	MergeSort(0,count-1);
+	finish = clock();
+	double duration = double(finish - start);
 	/*for (int i = 0; i < count-1; i++)
 	{
 		printf("%d ")
 	}*/
-	printf("done");
+	printf("%.0fms",duration);
 	return 0;
-
 }
